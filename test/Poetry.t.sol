9ee3cc7d212b8decca1487cb9dfb22c4be8bac63
Gerard Keiser <gerardkeiser@gmail.com>
2022-11-21 15:58:45 -0600

    Initial test passing

--- test/Poetry.t.sol
+++ test/Poetry.t.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Poetry.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract PoetryTest is Test {
+    Poetry public poetry;
 
     function setUp() public {
         poetry = new Poetry();
